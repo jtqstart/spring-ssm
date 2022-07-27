e83424d66ae878bee780f8e52b6922cc9829e65a
jtqstart <110037701+jtqstart@users.noreply.github.com>
2022-07-27 11:54:59 +0800

    修改了exercise

--- src/main/java/com/atguigu/java/exercise.java
+++ src/main/java/com/atguigu/java/exercise.java
@@ -8,6 +8,7 @@ public class exercise {
     public static void main(String[] args) {
         System.out.println("11111111111111111");
         System.out.println("22222222222222222");
-        System.out.println("33333333333333333");
+        System.out.println("99999999999999999");
+         System.out.println("8888888888888888");
     }
 }
