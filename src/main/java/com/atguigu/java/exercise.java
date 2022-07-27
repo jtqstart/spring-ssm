04f492b6c0ba3529e4f658ca2ab8d4291dbdf84a
jtqstart <15660167271@163.com>
2022-07-27 11:51:27 +0800

    测试

---
+++ src/main/java/com/atguigu/java/exercise.java
@@ -0,0 +1,13 @@
+package com.atguigu.java;
+
+/**
+ * @author jtqstart
+ * @create 2022-07-27 11:48
+ */
+public class exercise {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111");
+        System.out.println("22222222222222222");
+        System.out.println("33333333333333333");
+    }
+}
